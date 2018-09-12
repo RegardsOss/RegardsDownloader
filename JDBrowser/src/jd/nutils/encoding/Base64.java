cb30a1fc3aed35691cae969b1fe7fd9fe3521757
Unknown <olivier.rousselot@gmail.com>
2018-09-12 11:44:06 +0200

    Remove useless sub-modules

--- JDBrowser/src/jd/nutils/encoding/Base64.java
+++
@@ -1,5 +0,0 @@
-package jd.nutils.encoding;
-
-public class Base64 extends org.appwork.utils.encoding.Base64 {
-
-}
--- MyJDownloaderClient/src/org/jdownloader/myjdownloader/client/bindings/PluginConfigEntryStorable.java
+++
@@ -1,5 +0,0 @@
-package org.jdownloader.myjdownloader.client.bindings;
-
-public class PluginConfigEntryStorable extends AdvancedConfigEntryDataStorable {
-
-}
